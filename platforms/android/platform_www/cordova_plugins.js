3881ff7331a8b8ff3a5c76bf47dcd54d96313a3b
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2020-05-22 21:48:07 -0300

    Ok

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -62,20 +62,6 @@ module.exports = [
     ]
   },
   {
-    "id": "es6-promise-plugin.Promise",
-    "file": "plugins/es6-promise-plugin/www/promise.js",
-    "pluginId": "es6-promise-plugin",
-    "runs": true
-  },
-  {
-    "id": "cordova-plugin-x-socialsharing.SocialSharing",
-    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
-    "pluginId": "cordova-plugin-x-socialsharing",
-    "clobbers": [
-      "window.plugins.socialsharing"
-    ]
-  },
-  {
     "id": "cordova-clipboard.Clipboard",
     "file": "plugins/cordova-clipboard/www/clipboard.js",
     "pluginId": "cordova-clipboard",
@@ -89,6 +75,20 @@ module.exports = [
     "pluginId": "cordova-plugin-insomnia",
     "clobbers": [
       "window.plugins.insomnia"
+    ]
+  },
+  {
+    "id": "es6-promise-plugin.Promise",
+    "file": "plugins/es6-promise-plugin/www/promise.js",
+    "pluginId": "es6-promise-plugin",
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-x-socialsharing.SocialSharing",
+    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+    "pluginId": "cordova-plugin-x-socialsharing",
+    "clobbers": [
+      "window.plugins.socialsharing"
     ]
   }
 ];
@@ -106,10 +106,10 @@ module.exports.metadata =
   "cordova-promise-polyfill": "0.0.2",
   "cordova-admob-sdk": "0.24.1",
   "cordova-plugin-admob-free": "0.27.0",
-  "es6-promise-plugin": "4.2.2",
-  "cordova-plugin-x-socialsharing": "5.6.3",
   "cordova-clipboard": "1.3.0",
-  "cordova-plugin-insomnia": "4.3.0"
+  "cordova-plugin-insomnia": "4.3.0",
+  "es6-promise-plugin": "4.2.2",
+  "cordova-plugin-x-socialsharing": "6.0.0"
 };
 // BOTTOM OF METADATA
 });
