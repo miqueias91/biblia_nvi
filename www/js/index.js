9cbc021234ce96f681b89a2613728882c30a60b7
Miqueias <miqueias.caetano@doctum.edu.br>
2020-10-27 14:08:49 -0300

    Ok

--- www/js/index.js
+++ www/js/index.js
@@ -86,18 +86,45 @@ var app = {
     this.getIds();
   },
   oneSignal: function() {
-    window.plugins.OneSignal
-      .startInit('aa08ceb7-09b5-42e6-8d98-b492ce2e5d40')
-      .handleNotificationOpened(function(jsonData) {
-        var mensagem = JSON.parse(JSON.stringify(jsonData['notification']['payload']['body']));
 
-        ons.notification.alert(
-          mensagem,
-          {title: 'Ola!'}
-        );
-      })
-      .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
-      .endInit();
+    //START ONESIGNAL CODE
+    //Remove this method to stop OneSignal Debugging 
+    window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+
+    var notificationOpenedCallback = function(jsonData) {
+      alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+
+    // Set your iOS Settings
+    var iosSettings = {};
+    iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+    iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+
+    window.plugins.OneSignal
+    .startInit("aa08ceb7-09b5-42e6-8d98-b492ce2e5d40")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .iOSSettings(iosSettings)
+    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+    .endInit();
+  
+    // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
+    window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+      alert("Notificações aceitas pelo usuário: " + accepted);
+    });
+    //END ONESIGNAL CODE
+
+    // window.plugins.OneSignal
+    //   .startInit('aa08ceb7-09b5-42e6-8d98-b492ce2e5d40')
+    //   .handleNotificationOpened(function(jsonData) {
+    //     var mensagem = JSON.parse(JSON.stringify(jsonData['notification']['payload']['body']));
+
+    //     ons.notification.alert(
+    //       mensagem,
+    //       {title: 'Ola!'}
+    //     );
+    //   })
+    //   .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+    //   .endInit();
   },
   //FUNÇÃO DE BUSCA
   onSearchKeyDown: function(id) {
