2351dd7a5125f56613b3a21778036d518d5909fb
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2020-10-31 09:27:53 -0300

    ok

--- www/js/index.js
+++ www/js/index.js
@@ -136,6 +136,7 @@ var app = {
         title: 'Mensagem',
         callback: function (index) {
           if (0 == index) {
+            //OneSignal.clearOneSignalNotifications();
             fn.pushPage({'id': 'notificacao.html', 'title': 'Notificação||'+hash});
           }
         }
